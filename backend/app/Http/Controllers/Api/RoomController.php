af0f8002f54f9d181bc2b021944e3165bb3f11ba
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-02-25 15:25:34 +0530

    rooms

---
+++ backend/app/Http/Controllers/Api/RoomController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\RoomResource;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Resources\CourseResource;
+use App\Http\Controllers\Api\BaseController;
+
+class RoomController extends BaseController
+{
+    public function index(Request $request): JsonResponse
+    {
+        // Get the institute ID from the logged-in user's staff details.
+        $instituteId = Auth::user()->staff->institute_id;
+    
+        // Start the query by filtering staff based on the institute_id.
+        $query = Room::where('institute_id', $instituteId);
+    
+        // If there's a search term, apply additional filtering.
+        if ($request->query('search')) {
+            $searchTerm = $request->query('search');
+            $query->where(function ($query) use ($searchTerm) {
+                $query->where('room_name', 'like', '%' . $searchTerm . '%');
+            });
+        }
+    
+        // Paginate the results.
+        $room = $query->paginate(15);
+    
+        // Return the paginated response with staff resources.
+        return $this->sendResponse(
+            [
+                "Room" => RoomResource::collection($room),
+                'Pagination' => [
+                    'current_page' => $room->currentPage(),
+                    'last_page'    => $room->lastPage(),
+                    'per_page'     => $room->perPage(),
+                    'total'        => $room->total(),
+                ]
+            ],
+            "Room retrieved successfully"
+        );
+    }
+
+
+    public function store(Request $request): JsonResponse
+    {
+        
+        
+        // Create a new staff record and assign the institute_id from the logged-in admin
+        $room = new Room();
+        $room->institute_id = Auth::user()->staff->institute_id;  
+        $room->room_number = $request->input('room_number');
+        $room->room_name = $request->input('room_name');
+        $room->save();
+        
+        return $this->sendResponse([new RoomResource($room)], "Room stored successfully");
+    }
+
+
+    public function show(string $id): JsonResponse
+    {
+        $room = Room::find($id);
+
+        if(!$room){
+            return $this->sendError("Room not found", ['error'=>'Room not found']);
+        }
+
+  
+        return $this->sendResponse([ new RoomResource($room) ], "Room retrived successfully");
+    }
+
+
+    public function update(Request $request, string $id): JsonResponse
+    {
+ 
+        $room = Room::find($id);
+
+        if(!$room){
+            return $this->sendError("Room not found", ['error'=>'Room not found']);
+        }
+       
+                       
+        $room->institute_id = Auth::user()->staff->institute_id; // This will be 1 based on your admin login response
+        $room->room_number = $request->input('room_number');
+        $room->room_name = $request->input('room_name');
+           $room->save();
+       
+        return $this->sendResponse([ new RoomResource($room)], "Room updated successfully");
+
+    }
+
+
+    public function destroy(string $id): JsonResponse
+    {
+        $room = Room::find($id);
+        if(!$room){
+            return $this->sendError("Room not found", ['error'=> 'Room not found']);
+        }
+         $room->delete();
+         return $this->sendResponse([], "Room deleted successfully");
+    }
+}
---
+++ backend/database/migrations/2025_02_25_074715_create_rooms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('institute_id');
+            $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
+
+            // rooms fields
+            $table->string('room_number')->nullable();
+            $table->string('room_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
