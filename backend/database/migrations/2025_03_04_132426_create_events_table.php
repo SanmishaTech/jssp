d223153bb9c2974cf1652d980cb6732ccc2dfa06
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-03-04 18:56:28 +0530

    EventController-1

---
+++ backend/database/migrations/2025_03_04_132426_create_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('institute_id');
+            $table->foreign('institute_id')->references('id')->on('institutes')->onDelete('cascade');
+            
+            $table->string("venue")->nullable();
+            $table->string("date")->nullable();
+            $table->string("time")->nullable();
+            $table->string("synopsis")->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
